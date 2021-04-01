2dba988de3132e0e702764703a612fe36ad1a769
b0829065 <b0829065@cgu.edu.tw>
2021-04-01 20:39:57 +0800

    please...

--- quiz1.js
+++ quiz1.js
@@ -12,16 +12,3 @@ function myFunction(){
             ///也可以寫 document.body.appendChild(btn).addEventListener("click",change)
             ///# Listener by myFunction click  to do function change.
 }
-
-      /*function change() {     
-          var header = document.getElementById("aa");     
-              header.innerHTML = "CSIE@CGU";     
-          var para = document.getElementById("zz");     
-              para.innerHTML = "怎麼那麼棒！！.";   
-      }
-      function myFunction(){
-          var btn = document.createElement("BUTTON");
-              btn.innerHTML = "Change this document";
-              document.body.appendChild(btn)
-              btn.addEventListener("click",change);
-      }*/
